84ba33d8e4a8d1caf33837ddbf5a1fb36975084c
mohamed <mohamedbenlakhrech@gmail.com>
2018-11-26 14:29:36 +0100

    last

--- index.js
+++ index.js
@@ -7,7 +7,9 @@ app.use(express.static(path.join(__dirname, '/public')));
 app.get('/', (req, res) => {
   res.render('index.html');
 })
-
-app.listen(3000, () => {
-  console.log('Example app listenting on port 3000!');
+app.get('/hello', (req, res) => {
+  res.send('Hello world!!');
+})
+app.listen(4245, () => {
+  console.log('Example app listenting on port 4245!');
 });
